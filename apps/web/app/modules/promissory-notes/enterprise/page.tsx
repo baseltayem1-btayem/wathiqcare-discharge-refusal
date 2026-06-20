8a6e57d01940aedcc6b23d52a2784ffed34aa5c0
Basel Tayem <baseltayem1@gmail.com>
2026-06-20 06:15:13 +0300

    Integrate enterprise promissory notes module route

---
+++ apps/web/app/modules/promissory-notes/enterprise/page.tsx
@@ -0,0 +1,7 @@
+import WathiqNoteWorkflowModule from "@/components/modules/WathiqNoteWorkflowModule";
+
+export const dynamic = "force-dynamic";
+
+export default function ElectronicPromissoryNotesEnterprisePage() {
+  return <WathiqNoteWorkflowModule />;
+}
